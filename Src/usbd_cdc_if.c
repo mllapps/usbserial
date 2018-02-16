042da81d5462feb3f60b10384df27901eb58cfd8
fl0mll <mueller@mllapps.com>
2018-02-16 21:19:46 +0100

    Toggle LED1 on serial device input. Send 1 to power on and 0 to power off.

--- Src/usbd_cdc_if.c
+++ Src/usbd_cdc_if.c
@@ -291,6 +291,13 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
+	if(Buf[0] == '1')
+	{
+		HAL_GPIO_WritePin(LD1_OUT_GPIO_Port, LD1_OUT_Pin, GPIO_PIN_RESET);
+	}else if(Buf[0] == '0')
+	{
+		HAL_GPIO_WritePin(LD1_OUT_GPIO_Port, LD1_OUT_Pin, GPIO_PIN_SET);
+	}
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
   return (USBD_OK);
