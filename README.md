ea13aa04a5ccb73a3e4e884f2234bce4bf3f7e51
fl0mll <mueller@mllapps.com>
2018-02-16 21:19:16 +0100

    new generated code to use default value for the led1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# usbserial
+# usbserial cdc with stm32f103c8t6
+
 USB serial device CDC with stm32f103c8 board
+
+Coming soon...
--- Src/gpio.c
+++ Src/gpio.c
@@ -81,7 +81,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD1_OUT_GPIO_Port, LD1_OUT_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD1_OUT_GPIO_Port, LD1_OUT_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = LD1_OUT_Pin;
